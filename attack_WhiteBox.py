8fb938fdc88ffb587975de7de47eb0f8943e64b2
erichson <erichson@uw.edu>
2019-03-15 16:54:13 -0700

    adverserial attacks

---
+++ attack_WhiteBox.py
@@ -0,0 +1,291 @@
+from __future__ import print_function
+import argparse
+import time
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable, grad
+
+import scipy.io as lmd
+
+from attack_method import *
+from utils import *
+
+
+
+import os
+
+from advfuns import *
+from models import *
+
+
+from prettytable import PrettyTable
+import pandas as pd
+
+
+#==============================================================================
+# Attack settings
+#==============================================================================
+parser = argparse.ArgumentParser(description='Attack Example for MNIST')
+
+parser.add_argument('--test-batch-size', type=int, default=2000, metavar='N', help='input batch size for testing (default: 1000)')
+
+parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
+
+parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+
+parser.add_argument('--eps', type=float, default=0.01, metavar='E', help='how far to perturb input in the negative gradient sign')
+
+parser.add_argument('--arch', type=str, default='LeNetLike', help='choose an architecture')
+
+parser.add_argument('--resume', type=str, default='mnist_result/LeNetLike_baseline.pkl', help='choose an existing model')
+
+parser.add_argument('--dataset', type=str, default='mnist', help='chose dataset')
+
+parser.add_argument('--iter', type=int, default=40, help='number of iterations for FGSM')
+
+parser.add_argument('--iter_df', type=int, default=40, help='number of iterations for DeepFool')
+
+parser.add_argument('--jump', type=float, nargs='+', default=[0.0], help='jump value')
+
+parser.add_argument('--runs', type=int, default=1, help='number of simulations')
+
+parser.add_argument('--depth', type=int, default=20, help='choose the depth of resnet')
+#
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+torch.manual_seed(args.seed)
+
+
+
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
+train_loader, test_loader = getData(name=args.dataset, train_bs=args.test_batch_size, test_bs=args.test_batch_size)
+batchSize = args.test_batch_size
+
+
+
+for arg in vars(args):
+    print(arg, getattr(args, arg))
+
+
+if not os.path.exists('results'):
+    os.makedirs('results')
+
+#================================================
+# begin simulation
+#================================================
+accuracy = pd.DataFrame()
+relative = pd.DataFrame()
+absolute = pd.DataFrame()
+attack_time = pd.DataFrame()
+
+for jump in args.jump:
+    
+    
+    #================================================
+    # get model
+    #================================================
+    model_list = {
+            'LeNetLike': LeNetLike(jump = jump),
+            'AlexLike': AlexLike(jump = jump),
+            'JumpResNet': JumpResNet(depth=args.depth, jump = jump),
+            'MobileNetV2': MobileNetV2(jump = jump),      
+    }
+    
+    
+    model = model_list[args.arch]
+    if args.cuda:
+        model.cuda()
+    
+    model = torch.nn.DataParallel(model)
+    model.load_state_dict(torch.load(args.resume))
+    model.eval()
+    
+    
+    
+    #==============================================================================
+    # Begin attack
+    #==============================================================================
+    for irun in range(args.runs):
+        if args.dataset == 'mnist':
+            num_data = 10000
+            num_class = 9
+
+            X_ori = torch.Tensor(num_data, 1, 28, 28)
+            X_fgsm = torch.Tensor(num_data, 1, 28, 28)
+            X_deepfool1 = torch.Tensor(num_data, 1, 28, 28)
+            X_deepfool2 = torch.Tensor(num_data, 1, 28, 28)            
+            
+        elif args.dataset == 'cifar10':
+            num_data = 10000
+            num_class = 9
+            
+            X_ori = torch.Tensor(num_data, 3, 32, 32)
+            X_fgsm = torch.Tensor(num_data, 3, 32, 32)
+            X_deepfool1 = torch.Tensor(num_data, 3, 32, 32)
+            X_deepfool2 = torch.Tensor(num_data, 3, 32, 32)            
+
+        
+        iter_fgsm = 0.
+        iter_dp1 = 0.
+        iter_dp2 = 0.
+        
+        
+        Y_test = torch.LongTensor(num_data)
+        
+        
+        
+        
+        
+        print('Run IFGSM')
+        stat_time = time.time()
+        for i, (data, target) in enumerate(test_loader):
+        
+            X_ori[i*batchSize:(i+1)*batchSize, :] = data
+            Y_test[i*batchSize:(i+1)*batchSize] = target
+            
+            X_fgsm[i*batchSize:(i+1)*batchSize,:], a = fgsm_adaptive_iter(model, data, target, args.eps, iterations=args.iter)
+            #iter_fgsm += a
+        #print('iters: ', iter_fgsm)
+        time_ifgsm = time.time() - stat_time
+        print('total_time: ', time_ifgsm)
+            
+        
+        print('Run DeepFool (inf norm)')
+        stat_time = time.time()
+        for i, (data, target) in enumerate(test_loader):
+            X_deepfool1[i*batchSize:(i+1)*batchSize,:], a = deep_fool_iter(model, data, target, c=num_class, p=1, iterations=args.iter_df)
+            iter_dp1 += a
+        print('iters: ', iter_dp1)
+        time_deepfool_inf = time.time() - stat_time
+        print('total_time: ', time_deepfool_inf)
+        
+        
+        print('Run DeepFool (two norm)')
+        stat_time = time.time()
+        for i, (data, target) in enumerate(test_loader):
+            X_deepfool2[i*batchSize:(i+1)*batchSize,:], a = deep_fool_iter(model, data, target, c=num_class, p=2, iterations=args.iter_df)
+            iter_dp2 += a
+        print('iters: ', iter_dp2)
+        time_deepfool_two = time.time() - stat_time        
+        print('total_time: ', time_deepfool_two)
+        
+        
+        
+        result_acc = np.zeros(7)
+        result_ent = np.zeros(7)
+        result_dis = np.zeros(7)
+        result_dis_abs = np.zeros(7)
+        result_large = np.zeros(7)
+        
+        
+        result_acc[0], result_ent[0] = test_ori(model, test_loader, num_data, args)
+        result_acc[1], result_ent[1] = test_adv(X_fgsm, Y_test, model, num_data, args)
+        result_acc[2], result_ent[2] = test_adv(X_deepfool1, Y_test, model, num_data, args)
+        result_acc[3], result_ent[3] = test_adv(X_deepfool2, Y_test, model, num_data, args)
+        
+        
+        # FGSM inf norm
+        result_dis[1], result_dis_abs[1],  result_large[1]= distance(X_fgsm, X_ori, norm=1)
+        
+        # FGSM two norm
+        result_dis[2], result_dis_abs[2],  result_large[2]= distance(X_fgsm, X_ori, norm=2)
+        
+        # Deepfool (inf) inf norm
+        result_dis[3], result_dis_abs[3],  result_large[3]= distance(X_deepfool1, X_ori, norm=1)
+
+        # Deepfool (inf) two norm
+        result_dis[4], result_dis_abs[4],  result_large[4]= distance(X_deepfool1, X_ori, norm=2)
+                
+        # Deepfool (two) inf norm
+        result_dis[5], result_dis_abs[5],  result_large[5]= distance(X_deepfool2, X_ori, norm=1)
+        
+        # Deepfool (two) two norm
+        result_dis[6], result_dis_abs[6],  result_large[6]= distance(X_deepfool2, X_ori, norm=2)        
+        
+        
+        #***********************
+        # Print results
+        #***********************
+        print('Jump value: ', jump)
+        x = PrettyTable()
+        x.field_names = [" ", "Clean Data", "IFGSM", "DeepFool_inf", "DeepFool"]
+        x.add_row(np.hstack(('Accuracy: ',   np.round(result_acc[([0,1,2,3])], 5))))
+        x.add_row(np.hstack(('Rel. Noise: ', np.round(result_dis[([0,1,3,6])], 5))))
+        x.add_row(np.hstack(('Abs. Noise: ', np.round(result_dis_abs[([0,1,3,6])], 5))))
+        print(x)
+        
+        
+        #***********************
+        # Add to pandas df
+        #***********************
+        
+        
+        s = pd.Series({"jump" : jump, 
+                       "clean": np.round(result_acc[0], 3) , 
+                       "IFGSM_inf": np.round(result_acc[1], 3),
+                       "IFGSM_two": np.round(result_acc[1], 3), 
+
+                       "DeepFool_max_inf": np.round(result_acc[2], 3), 
+                       "DeepFool_max_two": np.round(result_acc[2], 3),
+
+                       "DeepFool_inf": np.round(result_acc[3], 3), 
+                       "DeepFool_two": np.round(result_acc[3], 3)                       
+                       })    
+        accuracy = accuracy.append(s, ignore_index=True)    
+        
+        
+        s = pd.Series({"jump" : jump, 
+                       "clean": np.round(result_dis[0], 3) , 
+                       "IFGSM_inf": np.round(result_dis[1], 3),
+                       "IFGSM_two": np.round(result_dis[2], 3), 
+
+                       "DeepFool_max_inf": np.round(result_dis[3], 3), 
+                       "DeepFool_max_two": np.round(result_dis[4], 3),
+
+                       "DeepFool_inf": np.round(result_dis[5], 3), 
+                       "DeepFool_two": np.round(result_dis[6], 3)                       
+                       }) 
+        relative = relative.append(s, ignore_index=True)    
+            
+            
+        s = pd.Series({"jump" : jump, 
+                       "clean": np.round(result_dis_abs[0], 3) , 
+                       "IFGSM_inf": np.round(result_dis_abs[1], 3),
+                       "IFGSM_two": np.round(result_dis_abs[2], 3), 
+
+                       "DeepFool_max_inf": np.round(result_dis_abs[3], 3), 
+                       "DeepFool_max_two": np.round(result_dis_abs[4], 3),
+
+                       "DeepFool_inf": np.round(result_dis_abs[5], 3), 
+                       "DeepFool_two": np.round(result_dis_abs[6], 3)                       
+                       }) 
+        absolute = absolute.append(s, ignore_index=True)    
+
+
+        s = pd.Series({"jump": jump, 
+                       "IFGSM": time_ifgsm, 
+                       "DeepFool_inf": time_deepfool_inf, 
+                       "DeepFool_two": time_deepfool_two})
+        attack_time = attack_time.append(s, ignore_index=True)    
+
+        if args.dataset == 'mnist':
+
+            accuracy.to_pickle("results/mnist_accuracy.pkl")
+            relative.to_pickle("results/mnist_relative.pkl")
+            absolute.to_pickle("results/mnist_absolute.pkl")
+            attack_time.to_pickle("results/mnist_time.pkl")
+            
+        elif args.dataset == 'cifar10':
+            accuracy.to_pickle("results/cifar_accuracy.pkl")
+            relative.to_pickle("results/cifar_relative.pkl")
+            absolute.to_pickle("results/cifar_absolute.pkl")
+            attack_time.to_pickle("results/cifar_time.pkl")
---
+++ attack_method.py
@@ -0,0 +1,159 @@
+##############################################################################
+## Attacks written by Zhewei Yao <zheweiy@berkeley.edu>
+#############################################################################
+
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable, grad
+
+import numpy as np
+
+
+#==============================================================================
+## FGSM
+#==============================================================================
+def fgsm(model, data, target, eps):
+    """Generate an adversarial pertubation using the fast gradient sign method.
+
+    Args:
+        data: input image to perturb
+    """
+    model.eval()
+    data, target = Variable(data.cuda(), requires_grad=True), target.cuda()
+    #data.requires_grad = True
+    model.zero_grad()
+    output = model(data)
+    loss = F.cross_entropy(output, target)
+    loss.backward(create_graph=False)
+    pertubation = eps * torch.sign(data.grad.data)
+    x_fgsm = data.data + pertubation
+    X_adv = torch.clamp(x_fgsm, torch.min(data.data), torch.max(data.data))
+
+    return X_adv
+
+
+def fgsm_iter(model, data, target, eps, iterations=10):
+    """
+    iteration version of fgsm
+    """
+    
+    X_adv = fgsm(model, data, target, eps)
+    for i in range(iterations):
+    	X_adv = fgsm(model, X_adv, target, eps)
+        
+    return X_adv
+
+
+
+
+def fgsm_adaptive_iter(model, data, target, eps, iterations):
+    update_num = 0
+    i = 0
+    while True:
+        if i >= iterations:
+            print('failed to fool all the image')
+            data = Variable(data)
+            break
+        
+        model.eval()
+        data, target = data.cuda(), target.cuda()
+        model.zero_grad()
+        output = model(data)
+
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        tmp_mask = pred.view_as(target) == target.data # get index
+        update_num += torch.sum(tmp_mask.long())
+        
+        #print(torch.sum(tmp_mask.long()))
+        if torch.sum(tmp_mask.long()) < 1: # allowed fail
+            break
+        
+        attack_mask = tmp_mask.nonzero().view(-1)
+        data[attack_mask,:] = fgsm(model, data[attack_mask,:], target[attack_mask], eps)
+        #data = fgsm(model, data, target, eps)
+        
+        i += 1
+        
+    return data.data, update_num
+
+
+
+
+
+
+
+
+#==============================================================================
+## Deep Fool
+#==============================================================================
+
+def deep_fool(model, data, c=9, p=2):
+    """Generate an adversarial pertubation using the dp method.
+
+    Args:
+        data: input image to perturb
+    """
+    model.eval()
+    data = data.cuda()
+    data.requires_grad = True
+    model.zero_grad()
+    output = model(data)
+    
+    output, ind = torch.sort(output, descending=True)
+    #c = output.size()[1]
+    n = len(data)
+
+    true_out = output[range(len(data)), n*[0]]
+    z_true = torch.sum(true_out)
+    data.grad = None
+    z_true.backward(retain_graph=True)
+    true_grad = data.grad
+    grads = torch.zeros([1+c] + list(data.size())).cuda()
+    pers = torch.zeros(len(data), 1+c).cuda()
+    for i in range(1,1+c):
+        z = torch.sum(output[:,i])
+        data.grad = None
+        model.zero_grad()
+        z.backward(retain_graph=True)
+        grad = data.grad # batch_size x 3k
+        grads[i] = grad.data
+        grad_diff = torch.norm(grad.data.view(n,-1) - true_grad.data.view(n,-1),p=p,dim=1) # batch_size x 1
+        pers[:,i] = (true_out.data - output[:,i].data)/grad_diff # batch_size x 1
+    pers[range(n),n*[0]] = np.inf
+    pers[pers < 0] = 0
+    per, index = torch.min(pers,1) # batch_size x 1
+    #print('maximum pert: ', torch.max(per))
+    update = grads[index,range(len(data)),:] - true_grad.data
+    
+    if p == 1:
+        update = torch.sign(update)
+    
+    elif p ==2:
+        update = update.view(n,-1)
+        update = update / (torch.norm(update, p=2, dim=1).view(n,1)+1e-6)
+    X_adv = data.data + torch.diag(torch.abs((per+1e-4)*1.02)).mm(update.view(n,-1)).view(data.size())
+    X_adv = torch.clamp(X_adv, torch.min(data.data), torch.max(data.data))
+    return X_adv
+
+
+
+def deep_fool_iter(model, data, target, c=9, p=2, iterations=10):
+    X_adv = data.cuda() + 0.0
+    update_num = 0.
+    for i in range(iterations):
+        model.eval()
+        Xdata, Xtarget = X_adv, target.cuda()
+        Xdata, Xtarget = Variable(Xdata, requires_grad=True), Variable(Xtarget)
+        model.zero_grad()
+        Xoutput = model(Xdata)
+        Xpred = Xoutput.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        tmp_mask = Xpred.view_as(Xtarget)==Xtarget.data # get index
+        update_num += torch.sum(tmp_mask.long())
+        #print('need to attack: ', torch.sum(tmp_mask))
+        if torch.sum(tmp_mask.long()) < 1:
+            break
+        #print (i, ': ', torch.sum(tmp_mask.long()))
+        attack_mask = tmp_mask.nonzero().view(-1)
+        X_adv[attack_mask,:] = deep_fool(model, X_adv[attack_mask,:], c=c, p=p)
+    model.zero_grad()
+    return X_adv, update_num
